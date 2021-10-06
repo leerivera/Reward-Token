c8572f5a538f205dd92fdbfeeebbbf34352c511f
leerivera <leerivera80@icloud.com>
2021-10-06 14:40:25 -0400

    migrations file completed

---
+++ src/contracts/Migration.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.5.0;
+
+contract Migrations {
+    address public string;
+    uint public last_completed_migration;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+;
+    function setCompleted(uint completed) public restriced {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public restriced {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
+
+}
